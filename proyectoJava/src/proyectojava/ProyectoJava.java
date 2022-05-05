827c89bfe79686ef1d732f5c100875efca6eb234
Crespin2 <jonathan.molina21@itca.edu.sv>
2022-05-04 20:09:18 -0600

    Ejercicio de Sitema de Control de Versiones #2

--- proyectoJava/src/proyectojava/ProyectoJava.java
+++ proyectoJava/src/proyectojava/ProyectoJava.java
@@ -2,7 +2,14 @@
 package proyectojava;
 
 public class ProyectoJava {
-
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private int edad;
+    
+    public static void main(String[] args) {
+        
+    }
 
     
     
